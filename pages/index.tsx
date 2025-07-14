70fe8a0703a26bdff84f3647a56e803f4f353fa6
sim-07 <simone07andreotti@gmail.com>
2025-07-14 12:21:19 +0200

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ export async function getStaticProps({ locale }: { locale: string }) {
                     'hero',
                     'about',
                     'login',
+                    'home'
                 ]
             )),
         },
