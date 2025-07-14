937d5a26db9decad128cfacc520bcb748d1448da
sim-07 <simone07andreotti@gmail.com>
2025-07-14 12:03:24 +0200

    modified:   next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 module.exports = {
     i18n: {
         locales: ['it', 'en'],
         defaultLocale: 'it',
-    }
-  }
+    },
+    localePath: path.resolve('./public/locales'),
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
+}
