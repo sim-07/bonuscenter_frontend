85c128c0c89c92926f83fce06a328d0240196c55
sim-07 <121951559+sim-07@users.noreply.github.com>
2025-12-07 00:07:01 +0100

    u

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 const { i18n } = require('./next-i18next.config')
 
 const nextConfig: NextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+
     devIndicators: false,
 
     i18n,
