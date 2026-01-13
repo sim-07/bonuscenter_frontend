6ee78018622080e8a2238f9a62e6f87167b1e5fd
sim-07 <121951559+sim-07@users.noreply.github.com>
2026-01-13 21:20:17 +0100

    modified:   src/components/bonus/BonusDesription.tsx

--- src/components/bonus/BonusDesription.tsx
+++ src/components/bonus/BonusDesription.tsx
@@ -9,6 +9,7 @@ import CommentsContainer from '../Comments/CommentsContainer';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import apiService from '../scripts/apiService';
+import { MDXRemote } from 'next-mdx-remote';
 
 interface BonusData {
     title: string;
@@ -41,6 +42,21 @@ export default function BonusDescription({ jsonData, mdxData }: Props) {
 
     const [auth, setAuth] = useState(false);
 
+    const mdxComponents = {
+        h2: (props: any) => <Typography variant="h5" fontWeight="bold" sx={{ mb: 3, mt: 5 }} {...props} />,
+        h3: (props: any) => <Typography variant="h6" fontWeight="bold" sx={{ mb: 2, mt: 4 }} {...props} />,
+        p: (props: any) => <Typography variant="body1" sx={{ mb: 2, lineHeight: 1.6 }} {...props} />,
+        ul: (props: any) => <Box component="ul" sx={{ pl: 4, mb: 2 }} {...props} />,
+        li: (props: any) => <Box component="li" sx={{ mb: 1 }}><Typography variant="body1" {...props} /></Box>,
+    };
+
+    const dataSummary = mdxData ? {
+        title: mdxData.frontmatter.title,
+        name: mdxData.frontmatter.name,
+        bonus_image: mdxData.frontmatter.bonus_image,
+        summary: mdxData.frontmatter.summary
+    } : jsonData;
+
     useEffect(() => {
         const checkAuth = async () => {
             try {
@@ -63,7 +79,7 @@ export default function BonusDescription({ jsonData, mdxData }: Props) {
     return (
         <>
             <Navbar>
-                {/* icona traduzioni */}
+                {/* TODO icona traduzioni */}
                 <Link
                     href={auth ? (locale === 'it' ? '/it/dashboard' : '/en/dashboard') : (locale === 'it' ? '/it/' : '/en/')}
                     style={{
@@ -75,126 +91,127 @@ export default function BonusDescription({ jsonData, mdxData }: Props) {
                 </Link>
             </Navbar>
 
-            {
-                jsonData ? (
-                    <Box sx={{
-                        p: 3,
-                        maxWidth: 1200,
-                        mx: 'auto',
-                        width: { sm: '95%', md: '80%' }
-                    }}>
-                        <Typography
-                            variant="h4"
-                            component="h1"
-                            gutterBottom
-                            fontWeight="bold"
-                            textAlign="left"
-                            sx={{
-                                mb: 5,
-                                mt: 3
+
+            <Box sx={{
+                p: 3,
+                maxWidth: 1200,
+                mx: 'auto',
+                width: { sm: '95%', md: '80%' }
+            }}>
+                <Typography
+                    variant="h4"
+                    component="h1"
+                    gutterBottom
+                    fontWeight="bold"
+                    textAlign="left"
+                    sx={{
+                        mb: 5,
+                        mt: 3
+                    }}
+                >
+                    {dataSummary?.title}
+                </Typography>
+
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Stack
+                        direction={{ md: "column", lg: "row" }}
+                        justifyContent="space-between"
+                        alignItems="center"
+                        width="80%"
+                        sx={{
+                            margin: '80px 0px'
+                        }}
+                    >
+                        <Image
+                            alt="bonusimage"
+                            src={`/images_bonus/${dataSummary?.bonus_image}.avif`}
+                            width={300}
+                            height={300}
+                            style={{
+                                borderRadius: '16px',
+                                marginBottom: '30px'
                             }}
-                        >
-                            {jsonData.title}
-                        </Typography>
+                        />
 
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Stack
-                                direction={{ md: "column", lg: "row" }}
-                                justifyContent="space-between"
-                                alignItems="center"
-                                width="80%"
-                                sx={{
-                                    margin: '80px 0px'
-                                }}
-                            >
-                                <Image
-                                    alt="bonusimage"
-                                    src={`/images_bonus/${jsonData.bonus_image}.avif`}
-                                    width={300}
-                                    height={300}
-                                    style={{
-                                        borderRadius: '16px',
-                                        marginBottom: '30px'
-                                    }}
-                                />
+                        <AllReferral bonusName={dataSummary?.name} />
+                    </Stack>
 
-                                <AllReferral bonusName={jsonData.name} />
-                            </Stack>
+                </Box>
 
-                        </Box>
+                <Box sx={{ mb: 5, p: 3, backgroundColor: 'grey.800', borderRadius: 3 }}>
+                    <Typography variant="h5" component="h2" gutterBottom fontWeight="medium" color="text.primary">
+                        {t('summary')}
+                    </Typography>
+                    <List disablePadding>
+                        <ListItem disableGutters>
+                            <ListItemText primary={t('welcome_bonus')} secondary={dataSummary?.summary?.bonus} />
+                        </ListItem>
+                        <ListItem disableGutters>
+                            <ListItemText primary={t('invite_bonus')} secondary={dataSummary?.summary?.invito} />
+                        </ListItem>
+                        <ListItem disableGutters>
+                            <ListItemText
+                                primary={t('required_deposit')}
+                                secondary={mdxData ? dataSummary?.summary?.deposito : dataSummary?.summary?.deposito_richiesto}
+                            />
+                        </ListItem>
+                        <ListItem disableGutters>
+                            <ListItemText primary={t('expiration')} secondary={dataSummary?.summary?.scadenza} />
+                        </ListItem>
+                        <ListItem disableGutters>
+                            <ListItemText primary={t('fees')} secondary={dataSummary?.summary?.commissioni} />
+                        </ListItem>
+                        <ListItem disableGutters>
+                            <ListItemText primary={t('extra_bonus')} secondary={dataSummary?.summary?.extra} />
+                        </ListItem>
+                        <ListItem disableGutters>
+                            <ListItemText primary={t('last_edit')} secondary={dataSummary?.summary?.last_edit} />
+                        </ListItem>
+                    </List>
+                </Box>
 
-                        <Box
-                            sx={{
-                                mb: 5,
-                                p: 3,
-                                backgroundColor: 'grey.800',
-                                borderRadius: 3,
-                            }}
-                        >
-                            <Typography variant="h5" component="h2" gutterBottom fontWeight="medium" color="text.primary">
-                                {t('summary')}
-                            </Typography>
-                            <List disablePadding>
-                                <ListItem disableGutters>
-                                    <ListItemText primary={t('welcome_bonus')} secondary={jsonData.summary.bonus} />
-                                </ListItem>
-                                <ListItem disableGutters>
-                                    <ListItemText primary={t('invite_bonus')} secondary={jsonData.summary.invito} />
-                                </ListItem>
-                                <ListItem disableGutters>
-                                    <ListItemText primary={t('required_deposit')} secondary={jsonData.summary.deposito_richiesto} />
-                                </ListItem>
-                                <ListItem disableGutters>
-                                    <ListItemText primary={t('expiration')} secondary={jsonData.summary.scadenza} />
-                                </ListItem>
-                                <ListItem disableGutters>
-                                    <ListItemText primary={t('fees')} secondary={jsonData.summary.commissioni} />
-                                </ListItem>
-                                <ListItem disableGutters>
-                                    <ListItemText primary={t('extra_bonus')} secondary={jsonData.summary.extra} />
-                                </ListItem>
-                                <ListItem disableGutters>
-                                    <ListItemText primary={t('last_edit')} secondary={jsonData.summary.last_edit} />
-                                </ListItem>
-                            </List>
-                        </Box>
+                <Divider sx={{ mb: 5 }} />
 
-                        <Divider sx={{ mb: 5 }} />
+                {
+                    jsonData ? (
+                        <Box>
 
-                        {jsonData.sections.map((section, i) => (
-                            <Box key={i} sx={{ mb: 6 }}>
-                                <Typography variant="h5" component="h3" gutterBottom fontWeight="bold" color="text.primary" sx={{ mb: 5 }}>
-                                    {section.title}
-                                </Typography>
-                                {section.content.map((p, idx) => (
-                                    <Typography key={idx} component="p" sx={{ mb: idx === section.content.length - 1 ? 0 : 2, lineHeight: 1.6 }}>
-                                        {p}
+                            {jsonData.sections.map((section, i) => (
+                                <Box key={i} sx={{ mb: 6 }}>
+                                    <Typography variant="h5" component="h3" gutterBottom fontWeight="bold" color="text.primary" sx={{ mb: 5 }}>
+                                        {section.title}
                                     </Typography>
-                                ))}
-                                {section.image && (
-                                    <Box
-                                        component="img"
-                                        src={section.image}
-                                        alt={section.title}
-                                        sx={{ width: '100%', maxWidth: 500, borderRadius: 2, mt: 3, boxShadow: 2 }}
-                                    />
-                                )}
-                            </Box>
-                        ))}
+                                    {section.content.map((p, idx) => (
+                                        <Typography key={idx} component="p" sx={{ mb: idx === section.content.length - 1 ? 0 : 2, lineHeight: 1.6 }}>
+                                            {p}
+                                        </Typography>
+                                    ))}
+                                    {section.image && (
+                                        <Box
+                                            component="img"
+                                            src={section.image}
+                                            alt={section.title}
+                                            sx={{ width: '100%', maxWidth: 500, borderRadius: 2, mt: 3, boxShadow: 2 }}
+                                        />
+                                    )}
+                                </Box>
+                            ))}
+                            
+                            <CommentsContainer bonusName={jsonData.name} />
+                        </Box>
+                    ) : (
+                        <></>
+                    )
+                }
 
-                        <CommentsContainer bonusName={jsonData.name} />
+                
 
-                    </Box>
-                ) : (
-                    <></>
-                )
-            }
-
+            </Box>
 
             <Footer />
         </>
