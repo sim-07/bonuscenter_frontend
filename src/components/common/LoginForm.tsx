fde7a2e425a6f1c671b9f8fb775510475cbd0f09
sim-07 <121951559+sim-07@users.noreply.github.com>
2026-02-07 11:14:42 +0100

    modified:   package.json 	modified:   pages/dashboard.tsx 	modified:   pages/profile.tsx 	modified:   pnpm-lock.yaml 	modified:   public/locales/en/login.json 	modified:   public/locales/it/login.json 	new file:   src/components/CustomIcons/CustomIcons.tsx 	modified:   src/components/bonus/UserReferral.tsx 	modified:   src/components/common/LoginForm.tsx 	new file:   src/lib/supabaseClient.ts

--- src/components/common/LoginForm.tsx
+++ src/components/common/LoginForm.tsx
@@ -6,17 +6,22 @@ import {
     TextField,
     Typography,
     Link as MuiLink,
-    CircularProgress
+    CircularProgress,
+    Divider,
+    Stack,
 } from '@mui/material';
 import router from 'next/router';
 import { useState } from 'react';
 import apiService from '@/components/scripts/apiService';
+import { supabase } from '@/lib/supabaseClient';
+import { GoogleIcon } from '@/components/CustomIcons/CustomIcons';
 
 import { useTranslation } from 'next-i18next';
 
 type LoginFormProps = {
     signinTypeP: string;
 };
+
 
 export default function LoginForm({ signinTypeP }: LoginFormProps) {
     const { t } = useTranslation('login');
@@ -41,6 +46,24 @@ export default function LoginForm({ signinTypeP }: LoginFormProps) {
         setErrorMessage('');
     };
 
+    const handleGoogleSignIn = async () => {
+        await supabase.auth.signOut();
+        if (typeof window !== 'undefined') {
+            window.localStorage.clear();
+        }
+
+        await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+                redirectTo: `${window.location.origin}/dashboard`,
+                queryParams: {
+                    access_type: 'offline',
+                    prompt: 'select_account',
+                },
+            },
+        });
+    };
+
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
@@ -54,6 +77,11 @@ export default function LoginForm({ signinTypeP }: LoginFormProps) {
         setIsLoading(true);
 
         try {
+            await supabase.auth.signOut();
+            if (typeof window !== 'undefined') {
+                window.localStorage.clear();
+            }
+
             const endpoint = signinType === 'login' ? 'login' : 'create_user';
             const data = await apiService("users", endpoint, formData);
 
@@ -76,8 +104,6 @@ export default function LoginForm({ signinTypeP }: LoginFormProps) {
 
     return (
         <Box
-            component="form"
-            onSubmit={handleSubmit}
             sx={{
                 display: 'flex',
                 flexDirection: 'column',
@@ -85,86 +111,134 @@ export default function LoginForm({ signinTypeP }: LoginFormProps) {
                 backgroundColor: "grey.800",
             }}
         >
-            {/* TITOLO LOGIN/SIGNUP */}
-            <Typography sx={{ color: 'grey.300', fontSize: '1.4em', mb: 3 }}>
-                {signinType === 'login' ? t('login') : t('signup')}
-            </Typography>
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
 
-            {formField.map((field, index) => {
-                return (
-                    field.formType.includes(signinType) && (
-                        <TextField
-                            key={index}
-                            label={field.label}
-                            type={field.type}
-                            variant="outlined"
-                            value={formData[field.name] || ''}
-                            onChange={(e) => handleChange(field.name, e.target.value)}
-                            required={field.required}
-                            sx={{ mb: 0.5 }}
-                        />
-                    )
-                );
-            })}
+            >
+                {/* TITOLO LOGIN/SIGNUP */}
+                <Typography sx={{ color: 'grey.200', fontSize: '1.7em', mb: 2 }}>
+                    {signinType === 'login' ? t('login') : t('signup')}
+                </Typography>
+
+                <Divider sx={{ my: 2 }} />
+
+                {/* INPUT */}
+                <Stack
+                    direction={"column"}
+                    gap={2}
+                    sx={{
+                        mt: 4,
+                    }}
+                >
+                    {formField.map((field, index) => {
+                        return (
+                            field.formType.includes(signinType) && (
+                                <TextField
+                                    key={index}
+                                    label={field.label}
+                                    type={field.type}
+                                    variant="outlined"
+                                    value={formData[field.name] || ''}
+                                    onChange={(e) => handleChange(field.name, e.target.value)}
+                                    required={field.required}
+                                    sx={{
+                                        '& .MuiOutlinedInput-root': {
+                                            borderRadius: '12px',
+                                            backgroundColor: 'rgba(255,255,255,0.03)',
+                                        },
+                                    }}
+                                />
+                            )
+                        );
+                    })}
+                </Stack>
+
+                <Stack direction={"column"} gap={1} sx={{ mt: 4 }}>
+
+
+                    {/* BOTTONE INVIO */}
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        disabled={isLoading}
+                        sx={{
+                            backgroundColor: 'primary.main',
+                            color: 'white',
+                            position: 'relative',
+                            textTransform: 'none',
+                            borderRadius: '12px',
+                        }}
+                    >
+                        {isLoading ? <CircularProgress size={24} color="inherit" /> : signinType === 'login' ? t('login_upper') : t('signup_upper')}
+                    </Button>
+
+
+                    {/* SWITCH LOGIN/SIGNUP */}
+                    <Typography sx={{ color: 'grey.400', display: 'inline-flex', alignItems: 'center', gap: 1, mt: 2 }}>
+                        {signinType === 'login' ? (
+                            <>
+                                {t('no_account')}
+                                <Button
+                                    variant="text"
+                                    sx={{ padding: 0, minWidth: 'auto', textTransform: 'none' }}
+                                    onClick={() => setSigninType('signup')}
+                                >
+                                    {t('signup')}
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                {t('already_registered')}
+                                <Button
+                                    variant="text"
+                                    sx={{ padding: 0, minWidth: 'auto', textTransform: 'none' }}
+                                    onClick={() => setSigninType('login')}
+                                >
+                                    {t('login')}
+                                </Button>
+                            </>
+                        )}
+                    </Typography>
+                </Stack>
+
+
+
+                {errorMessage && (
+                    <Typography sx={{ color: 'red' }}>
+                        {errorMessage}
+                    </Typography>
+                )}
+
+            </Box>
+
+            {/* GOOGLE */}
+            <Divider sx={{ color: "grey.200" }}>{t('or')}</Divider>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                <Button
+                    fullWidth
+                    variant="outlined"
+                    onClick={handleGoogleSignIn}
+                    startIcon={<GoogleIcon />}
+                    sx={{
+                        textTransform: 'none',
+                        borderRadius: '12px',
+                        py: 1.2
+                    }}
+                >
+                    {signinType == "signup" ? t("signingoogle") : t("logingoogle")}
+                </Button>
+            </Box>
 
             {/* PRIVACY POLICY */}
             {signinType === 'signup' && (
-                <Typography sx={{ color: "grey.400" }}>
+                <Typography sx={{ color: "grey.400", mt: 2, fontSize: "14px" }}>
                     {t('signup_privacy_prefix')}{' '}
-                    <MuiLink href="/privacy" sx={{ textDecoration: 'underline', cursor: 'pointer', marginLeft: '0px !important' }}>
+                    <MuiLink href="/privacy" sx={{ textDecoration: 'underline', cursor: 'pointer', marginLeft: '0px !important', fontSize: "14px" }}>
                         {t('privacy_policy')}
                     </MuiLink>.
                 </Typography>
             )}
-
-            {/* BOTTONE INVIO */}
-            <Button
-                type="submit"
-                variant="contained"
-                disabled={isLoading}
-                sx={{
-                    backgroundColor: 'primary.main',
-                    color: 'white',
-                    mt: 2,
-                    position: 'relative',
-                    p: 1
-                }}
-            >
-                {isLoading ? <CircularProgress size={24} color="inherit" /> : signinType === 'login' ? t('login_upper') : t('signup_upper')}
-            </Button>
-
-            {/* SWITCH LOGIN/SIGNUP */}
-            <Typography sx={{ color: 'grey.400', display: 'inline-flex', alignItems: 'center', gap: 1, mt: 2 }}>
-                {signinType === 'login' ? (
-                    <>
-                        {t('no_account')}
-                        <Button
-                            variant="text"
-                            sx={{ padding: 0, minWidth: 'auto', textTransform: 'none' }}
-                            onClick={() => setSigninType('signup')}
-                        >
-                            {t('signup')}
-                        </Button>
-                    </>
-                ) : (
-                    <>
-                        {t('already_registered')}
-                        <Button
-                            variant="text"
-                            sx={{ padding: 0, minWidth: 'auto', textTransform: 'none' }}
-                            onClick={() => setSigninType('login')}
-                        >
-                            {t('login')}
-                        </Button>
-                    </>
-                )}
-            </Typography>
-            {errorMessage && (
-                <Typography sx={{ color: 'red' }}>
-                    {errorMessage}
-                </Typography>
-            )}
-
         </Box>
     );
 }
